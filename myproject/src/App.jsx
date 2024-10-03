9931b66c258058af73dd76c546e146f82544af17
Jason Nazario <j-naz@nazd.us>
2024-10-03 08:16:54 -0700

    return coin function

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -1,6 +1,19 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+/*/ Create a vending machine application that takes in a 
+list of items and a 
+list of coins.
+// The vending machine should 
+allow the user to select an item and 
+insert coins to pay for the item.
+// The vending machine should 
+return the item if the user has inserted enough money.
+// If the user has not inserted enough money, 
+the vending machine should return the coins that were inserted.
+// The vending machine should also 
+return the item and any remaining change if the user has inserted more money than the item costs
+*/
 const testdata = {
   items: [
     {
@@ -33,41 +46,44 @@ const testdata = {
 }
 
 function VendingMachine() {
+  const [data, setData] = useState(structuredClone(testdata));
+
   const [cost, setCost] = useState(0);
   const [coins, setCoins] = useState(null);
   const [total, setTotal] = useState(0);
   const [returns, setReturns] = useState(null);
-  const [data, setData] = useState(testdata);
+  const [resetData,setResetData] = useState(false);
 
   useEffect(() => {
-    setCoins(testdata.coins.map((item) => {
+    (data.coins) && setCoins(data.coins.sort((a, b) => Number(b.value) - Number(a.value)).map((item) => {
       return item;
     }));
-    console.log(coins);
-    // const fetchData = async () => {
-    //   fetch('./fakeDta.json')
-    //     .then(res => res.json())
-    //     .then(data => setSchedules(data))
-    //     .finally(() => setLoading(false))
-    // };
-
-    // fetchData();
-  }, []);
+    
+  }, [resetData]);
   
+  function reset() {
+    setCost(0);
+    setTotal(0);
+    setData(testdata);
+    setResetData(!resetData);
+  }
+
   function handleItemClick(e) {
+    setReturns([]);
     setCost(e.target.value);
     console.log(e.target.value);
     // setCoins(coins + 1);
   }
   function handleCoinClick(e) {
-    setCoins(coins.map((item) => {
+    const newCoins = [...coins];
+    setCoins(newCoins.map((item) => {
       if (item.value == e.target.value)
       {
-        let coin = item;
-        coin.coin = (coin.coin) ? coin.coin + 1 : 1;
+        let con = item;
+        con.coin = (con.coin) ? con.coin + 1 : 1;
         
         setTotal(doTotalPaid);
-        return coin;
+        return con;
       } else
       {
         return item;
@@ -87,19 +103,26 @@ function VendingMachine() {
   }
   function deliverItem() {
     if (cost <= total) {
-      console.log("delivered");
-      alert("Change", Number(total) - Number(cost));
+      alert("Change" + (Number(total) - Number(cost)));
     }
   }
-  function returnCoins(amt) {
-    let remaining = amt;
+  // If the user has not inserted enough money, 
+  // the vending machine should return the coins that were inserted.
+  function returnCoins(e) { 
+    let remaining = e.target.value; // 1.00
     let coinStr = [];
-    coins && coins.map((item) => {
-      if (item.coin > 0 && item.value >= remaining) {
-        coinStr.push(`${item.name} x ${amt/item.value}`);
+
+    coins.map((item) => {
+      if (item.coin > 0 && item.value <= remaining)
+      {
+        const count = Math.floor(item.coin);
+        coinStr.push(`${ item.name } x ${ count }`);
+        // console.log("returned", count);
       }
     })
+    
     setReturns(coinStr);
+    reset();
   }
 
   return <>
@@ -109,7 +132,7 @@ function VendingMachine() {
         value={item.price}
       >{item.name} ${item.price}</button>
     })}
-    <div>Cost: {cost}</div>
+    <div>Cost: ${cost}</div>
     {/* <div><input value={cost} type="text" placeholder="Cost" /></div> */}
     <div>coins:</div>
     {coins && coins.map((item, i) => {
@@ -118,28 +141,31 @@ function VendingMachine() {
         value={item.value}
         disabled={(cost) < 0.05}>{item.name} ${item.value} x {item.coin}</button>
     })}
-    <br />
-    {(total) && <div>Total paid: ${total}</div>}
 
-    <button value={cost} onClick={deliverItem}>Pay now</button>
-    <button value={cost} onClick={returnCoins(total)}>Return</button>
+    <div>
+      {(total > 0) && `Total paid: ${total}`}
+    </div>
+    
+    <div>
+      <button value={cost} onClick={deliverItem}
+        disabled={(total) < 0.05}>Pay now</button>
+      {total &&
+        <button value={total} onClick={returnCoins}>Return</button>
+      }
+    </div>
 
     <div>return coins:</div>
-    <input type="text" />
+    {/* <input type="text" /> */}
     {returns && returns.map((item, i) => {
-      return <div key={i}>{item}</div>
+      return <div key={i} style={{color: "lightblue"}}>{item}</div>
     })}
   </>
 
 }
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <>
-      <VendingMachine />
-    </>
+    <VendingMachine />
   )
 }
 
