aad4f2eda10c400e2444f1d2ac8b48850383698f
Jason Nazario <j-naz@nazd.us>
2024-10-07 12:32:59 -0700

    coin return

--- myproject/src/utilities.js
+++ myproject/src/utilities.js
@@ -2,3 +2,7 @@ export function formatMoney(number) {
   return Number(number) // TODO: or the local currency, get it from the user's browser? No, must ask api.
     .toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 }
+
+export function roundCeil(number) {
+  return Math.ceil(number * 100) / 100;
+}
